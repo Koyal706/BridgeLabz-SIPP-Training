a6c721faed481cf581eb8a1967457d892b47df17
Koyal706 <koyalthokdar1234@gmail.com>
2025-06-16 12:45:52 +0530

    adding arrays folder in javacore branch

---
+++ Arrays/ques11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ques11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] salaries = new double[10];
+        int[] years = new int[10];
+        double[] bonus = new double[10];
+        double[] newSalaries = new double[10];
+        double totalBonus = 0, totalOldSalary = 0, totalNewSalary = 0
