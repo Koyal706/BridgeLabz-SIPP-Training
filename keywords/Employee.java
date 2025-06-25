a47bcf6500f9971a19d54f66c7f442b5c950e201
Koyal706 <koyalthokdar1234@gmail.com>
2025-06-25 12:22:53 +0530

    adding folder

---
+++ keywords/Employee.java
@@ -0,0 +1,28 @@
+class Employee {
+    static String companyName = "TechCorp";
+    static int totalEmployees = 0;
+
+    final int id;
+    String name;
+    String designation;
+
+    Employee(String name, int id, String designation) {
+        this.name = name;
+        this.id = id;
+        this.designation = designation;
+        totalEmployees++;
+    }
+
+    static void displayTotalEmployees() {
+        System.out.println("Total Employees: " + totalEmployees);
+    }
+
+    void displayEmployeeDetails() {
+        if (this instanceof Employee) {
+            System.out.println("Company: " + companyName);
+            System.out.println("Name: " + name);
+            System.out.println("ID: " + id);
+            System.out.println("Designation: " + designation);
+        }
+    }
+}
